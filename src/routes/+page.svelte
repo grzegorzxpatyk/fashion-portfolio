274c53bcda1ba0cbaeb8b3230d96b1e567d7d92c
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-08-18 00:42:05 +0200

    update layout of the website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,35 @@
 <script>
-    import '../app.css';
+	import '../app.css';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="h-screen scroll-smooth snap-y snap-mandatory overflow-x-hidden no-scrollbar">
+	<section class="h-screen max-h-screen w-screen bg-red-200 grid grid-cols-2 snap-start">
+		<div class="h-screen w-full flex flex-col justify-end items-start p-10 ">
+            <h2 class="text-4xl uppercase">portfolio</h2>
+            <h1 class="text-xl">Martyna Regucka</h1>
+        </div>
+        <div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=0" alt="something random, idk." /></div>
+	</section>
+	<section class="h-screen max-h-screen w-screen bg-red-200 grid grid-cols-2 snap-start">
+		<div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=1" alt="something random, idk." /></div>
+        <div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=2" alt="something random, idk." /></div>
+	</section>
+	<section class="h-screen max-h-screen w-screen bg-red-200 grid grid-cols-3 snap-start">
+		<div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=3" alt="something random, idk." /></div>
+		<div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=4" alt="something random, idk." /></div>
+        <div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=5" alt="something random, idk." /></div>
+	</section>
+	<section class="h-screen max-h-screen w-screen bg-red-200 snap-start">
+        <div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=6" alt="something random, idk." /></div>
+	</section>
+	<section
+		class="h-screen w-screen bg-emerald-200 flex flex-col justify-center items-center snap-start"
+	>
+		<div>
+			<h1 class="text-3xl">Next section</h1>
+		</div>
+		<div>
+			<h2 class="text-2xl">portfolio</h2>
+		</div>
+	</section>
+</main>
