8fa81ea6f58a574e8274bb49c552281e94eb4fb3
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-08-19 22:43:57 +0200

    update layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,25 @@
 </script>
 
 <main class="h-screen scroll-smooth snap-y snap-mandatory overflow-x-hidden no-scrollbar">
-	<section class="h-screen max-h-screen w-screen bg-red-200 grid grid-cols-2 snap-start">
-		<div class="h-screen w-full flex flex-col justify-end items-start p-10 ">
-            <h2 class="text-4xl uppercase">portfolio</h2>
-            <h1 class="text-xl">Martyna Regucka</h1>
-        </div>
-        <div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=0" alt="something random, idk." /></div>
+	<section class="h-screen w-screen flex justify-center items-center snap-start">
+		<h1 class="text-5xl uppercase line-through">portfolio</h1>
 	</section>
 	<section class="h-screen max-h-screen w-screen bg-red-200 grid grid-cols-2 snap-start">
-		<div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=1" alt="something random, idk." /></div>
-        <div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=2" alt="something random, idk." /></div>
+		<div class="h-screen w-full flex flex-col justify-end items-start p-10 select-none">
+			<h1 class="text-4xl">Martyna Regucka</h1>
+			<p class="text-justify">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor atque pariatur a ipsam sint
+				minus odit reprehenderit maiores deleniti accusantium, cupiditate vitae laborum incidunt
+				sed! Ut rerum a commodi dignissimos!
+			</p>
+		</div>
+		<div class="h-screen w-full">
+			<img
+				class="h-full w-full object-cover"
+				src="https://picsum.photos/1920/1080?random=0"
+				alt="something random, idk."
+			/>
+		</div>
 	</section>
 	<section class="h-screen max-h-screen w-screen bg-red-200 grid grid-cols-3 snap-start">
 		<div class="h-screen w-full"><img class="h-full w-full object-cover" src="https://picsum.photos/1920/1080?random=3" alt="something random, idk." /></div>
