fb5aecaa103609e8df30e8e64f236d2377cc4856
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-08-20 01:58:20 +0200

    fix styling

--- src/lib/PortfolioSection.svelte
+++ src/lib/PortfolioSection.svelte
@@ -2,14 +2,14 @@
     export let images: Array<{src: string, alt: string}> | undefined = undefined;
 </script>
 
-{#if images && images.length > 0}
-<section class="h-screen w-screen grid grid-cols-{images.length} gap-2 snap-start">
+{#if images && images.length > 1}
+<section class="h-screen w-screen grid grid-cols-{images.length} snap-start px-2">
     {#each images as image}
-    <div class="h-full w-full"><img class="h-full w-full object-cover" src={image.src} alt={image.alt} /></div>
+    <div class="h-full w-full p-4 ps-2 pe-2"><img class="h-full w-full object-cover" src={image.src} alt={image.alt} /></div>
     {/each}
 </section>
-{:else if images && images.length === 0}
-<section class="h-screen w-screen snap-start">
+{:else if images && images.length === 1}
+<section class="h-screen w-screen snap-start p-4">
     <div class="h-full w-full"><img class="h-full w-full object-cover" src={images[0].src} alt={images[0].alt} /></div>
 </section>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,19 @@
 </script>
 
 <main class="h-screen scroll-smooth snap-y snap-mandatory overflow-x-hidden no-scrollbar">
-	<section class="h-screen w-screen flex justify-center items-center snap-start">
+	<section class="h-screen w-screen flex justify-center items-center snap-start bg-yellow-200">
 		<h1 class="text-5xl uppercase line-through">portfolio</h1>
 	</section>
 	<section class="h-screen max-h-screen w-screen bg-red-200 grid grid-cols-2 snap-start">
 		<div class="h-screen w-full flex flex-col justify-end items-start p-10 select-none">
-			<h1 class="text-4xl">Martyna Regucka</h1>
+			<h1 class="text-4xl mb-2">Martyna Regucka</h1>
 			<p class="text-justify">
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor atque pariatur a ipsam sint
 				minus odit reprehenderit maiores deleniti accusantium, cupiditate vitae laborum incidunt
 				sed! Ut rerum a commodi dignissimos!
 			</p>
 		</div>
-		<div class="h-screen w-full">
+		<div class="h-screen w-full p-12">
 			<img
 				class="h-full w-full object-cover"
 				src="https://picsum.photos/1920/1080?random=0"
@@ -41,13 +41,13 @@
 		images={[{ src: 'https://picsum.photos/1920/1080?random=6', alt: 'something random' }]}
 	/>
 	<section
-		class="h-screen w-screen bg-emerald-200 flex flex-col justify-center items-center snap-start"
+		class="h-screen w-screen bg-emerald-200 flex flex-col justify-center items-center snap-start select-none"
 	>
 		<div>
-			<h1 class="text-3xl">Next section</h1>
+			<h1 class="text-3xl">Contact</h1>
 		</div>
 		<div>
-			<h2 class="text-2xl">portfolio</h2>
+			<h2 class="text-2xl">instagram: martyna.regucka</h2>
 		</div>
 	</section>
 </main>
