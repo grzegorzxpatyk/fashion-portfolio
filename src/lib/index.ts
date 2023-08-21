6c9def50ebf54b3f82d2e3ccda79a96e25d98bfe
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-08-22 00:21:54 +0200

    add some photos

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -12,6 +12,14 @@ import photo10 from './images/photo-10.jpg';
 import photo11 from './images/photo-11.jpg';
 import photo12 from './images/photo-12.jpg';
 import photo33 from './images/photo-33.jpg';
+import photo54 from './images/photo-54.jpg';
+import photo58 from './images/photo-58.jpg';
+import photo60 from './images/photo-60.jpg';
+import photo63 from './images/photo-63.jpg';
+import photo64 from './images/photo-64.jpg';
+import photo69 from './images/photo-69.jpg';
+import photo73 from './images/photo-73.jpg';
+import photo118 from './images/photo-118.jpg';
 
 export const PortfolioSection = PortfolioSection__SvelteComponent_;
 
@@ -28,5 +36,13 @@ export const photos = {
 	photo10,
 	photo11,
     photo12,
-    photo33
+    photo33,
+	photo54,
+	photo58,
+	photo60,
+	photo63,
+	photo64,
+	photo69,
+	photo73,
+	photo118,
 };
