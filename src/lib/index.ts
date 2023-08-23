1e8167db8db636f9910406dbdad96e9889ac9fb4
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-08-23 22:28:58 +0200

    import all images with random order

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,50 +1,5 @@
 import PortfolioSection__SvelteComponent_ from './PortfolioSection.svelte';
-import photo1 from './images/photo-1.jpg';
-import photo2 from './images/photo-2.jpg';
-import photo3 from './images/photo-3.jpg';
-import photo4 from './images/photo-4.jpg';
-import photo5 from './images/photo-5.jpg';
-import photo6 from './images/photo-6.jpg';
-import photo7 from './images/photo-7.jpg';
-import photo8 from './images/photo-8.jpg';
-import photo9 from './images/photo-9.jpg';
-import photo10 from './images/photo-10.jpg';
-import photo11 from './images/photo-11.jpg';
-import photo12 from './images/photo-12.jpg';
-import photo33 from './images/photo-33.jpg';
-import photo54 from './images/photo-54.jpg';
-import photo58 from './images/photo-58.jpg';
-import photo60 from './images/photo-60.jpg';
-import photo63 from './images/photo-63.jpg';
-import photo64 from './images/photo-64.jpg';
-import photo69 from './images/photo-69.jpg';
-import photo73 from './images/photo-73.jpg';
-import photo118 from './images/photo-118.jpg';
 import AboutMe from './AboutMe.svelte';
 
 export const PortfolioSection = PortfolioSection__SvelteComponent_;
 export const AboutMeSection = AboutMe;
-
-export const photos = {
-	photo1,
-	photo2,
-	photo3,
-	photo4,
-	photo5,
-	photo6,
-	photo7,
-	photo8,
-	photo9,
-	photo10,
-	photo11,
-    photo12,
-    photo33,
-	photo54,
-	photo58,
-	photo60,
-	photo63,
-	photo64,
-	photo69,
-	photo73,
-	photo118,
-};
