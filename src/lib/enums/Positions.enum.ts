b1b8b30f8a884ad86fcffbc098e9a7e065c81f1e
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-08-25 01:10:27 +0200

    create Positions enum

---
+++ src/lib/enums/Positions.enum.ts
@@ -0,0 +1,11 @@
+export enum Positions {
+    BOTTOM = 'object-bottom',
+    CENTER = 'object-center',
+    LEFT = 'object-left',
+    LEFT_BOTTOM = 'object-left-bottom',
+    LEFT_TOP = 'object-left-top',
+    RIGHT = 'object-right',
+    RIGHT_BOTTOM = 'object-right-bottom',
+    RIGHT_TOP = 'object-right-top',
+    TOP = 'object-top'
+}
