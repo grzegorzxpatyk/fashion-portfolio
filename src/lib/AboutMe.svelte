4b330a5520f640a5445c4b68a5842f52669280ca
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-08-25 23:49:51 +0200

    update layout of AboutMe section

--- src/lib/AboutMe.svelte
+++ src/lib/AboutMe.svelte
@@ -1,34 +1,42 @@
 <script lang="ts">
-	import { InstagramLogo } from "radix-icons-svelte";
+	import { ChevronDown, InstagramLogo } from 'radix-icons-svelte';
 
-    export let portrait: string;
+	export let portrait: string;
 </script>
 
-<section class="h-screen max-h-screen w-screen bg-red-200 grid grid-cols-2 snap-start">
-    <div class="h-screen w-full flex flex-col justify-end items-start p-6 md:p-10 select-none">
-        <div class="w-full flex flex-row justify-between items-center mb-4">
-            <h1 class="text-4xl">Martyna Regucka</h1>
-            <a
-                href="https://www.instagram.com/martynaregucka/"
-                target="_blank"
-                class="hidden md:flex flex-row justify-center items-center"
-            >
-                <InstagramLogo class="mr-1" /> martynaregucka
-            </a>
-        </div>
-        <p class="text-justify text-sm md:text-base">
-            Innovative fashion designer and stylist, graduate of the Artistic School of Fashion Design.
-            Proven track record collaborating with diverse industry professionals, from designers and
-            photographers to managers and creative directors. Renowned for a keen sense of style,
-            efficiency, and productivity in translating concepts into captivating collections. Ready to
-            bring fresh, trendsetting ideas to your brand.
-        </p>
-    </div>
-    <div class="h-screen w-full lg:p-12">
-        <img
-            class="h-full w-full object-cover object-top"
-            src={portrait}
-            alt="Portrait of a beautiful girl."
-        />
-    </div>
+<section class="h-screen w-screen bg-white snap-start">
+    <header class="h-[15vh] flex justify-center items-center">
+		<h1 class="text-5xl uppercase font-extralight">portfolio</h1>
+	</header>
+	<div class="h-[85vh] grid grid-cols-3">
+		<div class="h-full w-full flex flex-col justify-end items-start p-6 pr-3 md:p-10 md:pr-5 col-span-2">
+			<div class="w-full flex flex-row justify-between items-center mb-4">
+				<h1 class="text-4xl">Martyna Regucka</h1>
+				<a
+					href="https://www.instagram.com/martynaregucka/"
+					target="_blank"
+					class="hidden md:flex flex-row justify-center items-center"
+				>
+					<InstagramLogo class="mr-1" /> martynaregucka
+				</a>
+			</div>
+			<p class="text-justify text-sm md:text-base mb-6">
+				Innovative fashion designer and stylist, graduate of the Artistic School of Fashion Design.
+				Proven track record collaborating with diverse industry professionals, from designers and
+				photographers to managers and creative directors. Renowned for a keen sense of style,
+				efficiency, and productivity in translating concepts into captivating collections. Ready to
+				bring fresh, trendsetting ideas to your brand.
+			</p>
+            <div class="w-full flex justify-center items-center">
+                <ChevronDown size={30} />
+            </div>
+		</div>
+		<div class="h-[85vh] w-full p-6 pl-3 md:p-10 md:pl-5">
+			<img
+				class="h-full w-full object-cover object-top"
+				src={portrait}
+				alt="Portrait of a beautiful girl."
+			/>
+		</div>
+	</div>
 </section>
