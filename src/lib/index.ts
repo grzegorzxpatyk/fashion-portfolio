33d57faa13d954f94092ca5598a6598474088638
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-08-27 23:24:29 +0200

    update imports/exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,9 @@
 import PortfolioSection__SvelteComponent_ from './PortfolioSection.svelte';
-import AboutMe from './AboutMe.svelte';
+import AboutMe__SvelteComponent_ from './AboutMe.svelte';
+import ScrollDownButton__SvelteComponent_ from './ScrollDownButton.svelte';
+import Qr__SvelteComponent_ from './qr.svelte';
 
 export const PortfolioSection = PortfolioSection__SvelteComponent_;
-export const AboutMeSection = AboutMe;
+export const AboutMeSection = AboutMe__SvelteComponent_;
+export const ScrollDownButton = ScrollDownButton__SvelteComponent_;
+export const QRCode = Qr__SvelteComponent_;
