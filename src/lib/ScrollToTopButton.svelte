1c0d468fbf8135c2fbb2a793e1fb8428ae82b93d
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-09-28 12:28:15 +0200

    create ScrollToTopButton component

---
+++ src/lib/ScrollToTopButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { ChevronUp } from 'radix-icons-svelte';
+
+	function scrollToTop() {
+		document.querySelector('main')?.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'auto'
+		});
+	}
+</script>
+
+<button on:click={scrollToTop} class="fixed bottom-4 right-4 rounded-full p-6 cursor-crosshair backdrop-blur-md backdrop-brightness-125 backdrop-hue-rotate-180 before:content-['top'] before:uppercase before:hidden before:opacity-0 before:h-0 hover:before:block hover:before:opacity-100 hover:before:h-8 before:transition-all before:duration-300 hover:before:animate-show-stretch"><ChevronUp size={30} /></button>
