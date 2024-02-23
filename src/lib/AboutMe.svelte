7b66262afa18144454a4878972fb3338f772418e
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2024-02-23 03:47:51 +0100

    refactor: clean up

--- src/lib/AboutMe.svelte
+++ src/lib/AboutMe.svelte
@@ -75,20 +75,18 @@
 			<div class="h-full w-full">
 				<img
 					class="h-full w-full object-scale-down portrait:max-md:backface-hidden {isPortraitFlipping &&
-						isCardOnPortraitSide &&
-						'animate-flip-y'} {isPortraitFlipping &&
-						!isCardOnPortraitSide &&
-						'animate-flip-y-reverse'} {isCardOnPortraitSide ? 'rotate-y-0' : 'rotate-y-180'}"
+						(isCardOnPortraitSide
+							? 'animate-flip-y'
+							: 'animate-flip-y-reverse')} {isCardOnPortraitSide ? 'rotate-y-0' : 'rotate-y-180'}"
 					src={portrait}
 					alt="Portrait of a beautiful girl."
 					on:click={handleAvatarClick}
 					on:animationend={handleAvatarAnimationEnd} />
 				<p
 					class="hidden portrait:max-md:block h-full overflow-y-auto overflow-x-hidden w-full px-6 text-justify text-lg relative -top-full left-[50%] translate-x-[-50%] backface-hidden {isPortraitFlipping &&
-						isCardOnPortraitSide &&
-						'animate-flip-y-reverse'} {isPortraitFlipping &&
-						!isCardOnPortraitSide &&
-						'animate-flip-y'} {isCardOnPortraitSide ? 'rotate-y-180' : 'rotate-y-0'}"
+						(isCardOnPortraitSide
+							? 'animate-flip-y-reverse'
+							: 'animate-flip-y')} {isCardOnPortraitSide ? 'rotate-y-180' : 'rotate-y-0'}"
 					on:click={handleAvatarClick}>
 					{bioDescription}
 				</p>
